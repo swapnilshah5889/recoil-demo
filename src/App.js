95bfe27c945076eb564e245962484c776af0ed0f
Swapnil Shah <swapnilshah5889@gmail.com>
2023-08-03 13:24:39 -0400

    Added recoil library and implemented RecoilRoot

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MyCard from './components/card.component';
 import MyTextField from './components/textfield.component';
 import { useState } from 'react';
+import { RecoilRoot } from 'recoil';
 
 
 function App() {
@@ -10,11 +11,12 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-
-    <div className="App" style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"space-evenly"}}>
-      <MyCard count={count} setCount={setCount}/>
-      <MyTextField count={count}/>
-    </div>
+    <RecoilRoot>
+      <div className="App" style={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"space-evenly"}}>
+        <MyCard count={count} setCount={setCount}/>
+        <MyTextField count={count}/>
+      </div>
+    </RecoilRoot>
   );
 }
 
